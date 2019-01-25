a1c2e1d7c9e63ce1543c3b73d8705c0c519e1fe7
halomademeapc <opr9029@gmail.com>
2019-01-24 23:21:41 -0500

    Add login button

---
+++ TheSongList/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheSongList.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet, Authorize]
+        public IActionResult Login(string returnUrl) => Redirect(returnUrl);
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            var target = $"{Request.Scheme}://{Request.Host}";
+            return Redirect($"https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue={target}");
+        }
+    }
+}
